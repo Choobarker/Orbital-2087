40ab26d3316471d1669c3655ad06ec15859dd968
Choobarker <runforeva@gmail.com>
2018-04-05 16:04:20 +1200

    Update enemy spawning
    Enemies now spawn on predefined spawn points

---
+++ EnemiesSpawning/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+	public float health = 2f;
+	public float speed = 2f;
+	private bool inPosition = false;
+	public float moveTime = 2f;
+	private float timeMoved = 0f;
+	
+	void start()
+	{
+		Debug.Log("Enemy Behaviour started");
+	}
+	
+	void Update () 
+	{		
+		while(!inPosition)
+		{
+			transform.position += Vector3.forward * Time.deltaTime * speed;
+			timeMoved += Time.deltaTime;
+
+			if(timeMoved >= moveTime)
+			{
+				inPosition = true;
+			}
+		}
+	}
+}
--- EnemiesSpawning/Assets/Scripts/WaveSpawner.cs
+++ EnemiesSpawning/Assets/Scripts/WaveSpawner.cs
@@ -15,16 +15,15 @@ public class WaveSpawner : MonoBehaviour {
 		public float delay;
 	}
 
+	public Transform[] spawnPoints;
 	public Wave[] waves;
 	private int nextWave = 0;
 
 	public float timeBetweenWaves = 5f;
 	private float waveCountDown = 0;
-
 	private float searchCountDown = 1f;
 
 	private SpawnState state = SpawnState.COUNTING;
-
 	private System.Random rand = new System.Random();
 
 	void Start()
@@ -38,13 +37,7 @@ public class WaveSpawner : MonoBehaviour {
 		{
 			if(!EnemyIsAlive())
 			{
-				Debug.Log("Wave Completed");
-
-				nextWave++;
-				if(nextWave > waves.Length)
-				{
-					nextWave = 0;
-				}
+				WaveCompleted();
 			}
 			else
 			{
@@ -57,13 +50,26 @@ public class WaveSpawner : MonoBehaviour {
 			if(state != SpawnState.SPAWNING)
 			{
 				StartCoroutine(SpawnWave(waves[nextWave]));
-
-				waveCountDown = timeBetweenWaves;
 			}			
 		}
 		else
 		{
 			waveCountDown -= Time.deltaTime;
+		}
+	}
+
+	void WaveCompleted()
+	{
+		Debug.Log("Wave Completed");
+
+		state = SpawnState.COUNTING;
+		waveCountDown = timeBetweenWaves;
+
+		nextWave++;
+		if(nextWave >= waves.Length)
+		{
+			nextWave = 0;
+			Debug.Log("All Waves Complete");
 		}
 	}
 
@@ -101,10 +107,18 @@ public class WaveSpawner : MonoBehaviour {
 	void SpawnEnemy(Transform enemy)
 	{
 		Debug.Log("Spawning Enemy: " + enemy.name);
+
+		// Give the enemy a predefined spawn point
+		Transform sp = spawnPoints[Random.Range(0, spawnPoints.Length)];
+		Instantiate(enemy, sp.position, sp.rotation);
+
+		// Generage random enemy spawn locations within an area
+		/*
 		float x = rand.Next(-10, 10);
 		float y = rand.Next(-10, 10);
 		float z = 0;
 
 		Instantiate(enemy, new Vector3(x, y, z), transform.rotation);
+		 */
 	}
 }
