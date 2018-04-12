82092afd14021cf8bf3a32d489a988c0144640ef
Aidan Milne-Reeves <svx5484@autuni.ac.nz>
2018-04-13 11:59:23 +1200

    Udated movement

--- Orbital-2087/Assets/Scripts/PlayerMovement.cs
+++ Orbital-2087/Assets/Scripts/PlayerMovement.cs
@@ -5,19 +5,28 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public bool playerButtonControl;
     public float speed;
     //public float tilt; //Unused atm
     public float circumference;
     private float moveVar;
-    public Transform target;
+    public Transform earth;
 
     void Update()
     {
     }
     private void FixedUpdate()
     {
-        //Controls player movement
-        moveVar += CrossPlatformInputManager.GetAxis("Horizontal") * Time.deltaTime * speed;//RA Remove inputgetaxis when building android ver.
+        //Controls player movement //TODO Tidy up 
+        if (playerButtonControl)
+        {
+            moveVar += CrossPlatformInputManager.GetAxis("Horizontal") * Time.deltaTime * speed; //RA Remove inputgetaxis when building android ver.
+        }
+        else if (!playerButtonControl)
+        {
+            //Vector3 tilt = Input.acceleration * Time.deltaTime * speed; //Accelerometer
+            moveVar += Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        }
         float x = Mathf.Sin(moveVar) * circumference;
         float y = Mathf.Cos(moveVar) * circumference;
         float z = 0;
@@ -25,10 +34,11 @@ public class PlayerMovement : MonoBehaviour
         transform.position = move;
         
         //Controls player rotation
-        Vector2 direction = new Vector2(target.position.x - move.x, target.position.y - move.y);
+        Vector2 direction = new Vector2(earth.position.x - move.x, earth.position.y - move.y);
         float rotation = (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg) + 90;
         this.transform.eulerAngles = new Vector3(0, 0, rotation);
         
+
         //TODO Tilt?
         //GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
     }
