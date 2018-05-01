522eb908e526d68a76dec8cdb680111d1faa5145
Malkay <mal.troyfleet@hotmail.com>
2018-05-02 11:31:52 +1200

    Updated player shooting
    Player shooting control has been added to its own script ShootProjectile.

--- Orbital-2087/Assets/Scripts/PlayerMovement.cs
+++ Orbital-2087/Assets/Scripts/PlayerMovement.cs
@@ -12,20 +12,7 @@ public class PlayerMovement : MonoBehaviour
     private float moveVar;
     public Transform earth;
 
-    public GameObject projectile;
-    public Transform projectileSpawn;
-    public float fireRate;
-    private float nextFire;
 
-
-    void Update()
-    {
-        if (Input.GetButton("Fire1") && Time.time > nextFire)
-        {
-            nextFire = Time.time + fireRate;
-            Instantiate(projectile, projectileSpawn.position, projectileSpawn.rotation);
-        }
-    }
 
     private void FixedUpdate()
     {
@@ -37,7 +24,7 @@ public class PlayerMovement : MonoBehaviour
         else if (!playerJoystickControl)
         {
             moveVar += Input.acceleration.x * Time.deltaTime * speed; //Accelerometer
-            //moveVar += Input.GetAxis("Horizontal") * Time.deltaTime * speed;//RA Remove inputgetaxis when building android ver.
+            moveVar += Input.GetAxis("Horizontal") * Time.deltaTime * speed;//RA Remove inputgetaxis when building android ver.
         }
         float x = Mathf.Sin(moveVar) * radius;
         float y = Mathf.Cos(moveVar) * radius;
---
+++ Orbital-2087/Assets/Scripts/ShootProjectile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootProjectile : MonoBehaviour {
+
+    public GameObject projectile;
+    public Transform projectileSpawn;
+    public float fireRate;
+    private float nextFire;
+
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(projectile, projectileSpawn.position, projectileSpawn.rotation);
+        }
+    }
+}
