ebe59ad1563e3d1d8bd63b9fc6d29ed0decfb800
Choobarker <runforeva@gmail.com>
2018-05-11 12:14:59 +1200

    Update file names and add boost sprites

---
+++ Orbital-2087/Assets/Scripts/FireRateBoost.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireRateBoost : MonoBehaviour 
+{
+	GameObject player;
+	
+
+    void Start()
+	{
+
+	}
+
+	void OnMouseDown()
+	{
+		Debug.Log("clicked");
+	}	
+}
