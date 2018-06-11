3825235aa57a50e7e7bb00dcc0f536ae559041e9
Choobarker <runforeva@gmail.com>
2018-06-11 20:51:39 +1200

    Made seperate scripts for each menu. and removed the settings menu from the main menu

---
+++ Orbital-2087/Assets/Scripts/PauseButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseButton : MonoBehaviour 
+{
+    Button pauseButton;
+    GameObject pauseMenu;
+
+	void Start () 
+    {
+		pauseButton = GetComponent<Button>();
+        pauseButton.onClick.AddListener(PauseClicked);
+	}
+	
+	private void PauseClicked()
+    {
+
+    }
+}
