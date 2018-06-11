7db122fb29c7985bd03b3d020f222c0398031ea2
Choobarker <runforeva@gmail.com>
2018-06-11 20:59:06 +1200

    Revert "Made seperate scripts for each menu. and removed the settings menu from the main menu"
    This reverts commit 3825235aa57a50e7e7bb00dcc0f536ae559041e9.

---
+++ Orbital-2087/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour 
+{
+    public Sprite pauseSprite;
+    public Sprite playSprite;
+    public AudioSource Audio;
+    public AudioSource AlienBullets;
+    public AudioSource playerBullets;
+    public AudioSource Earth;
+    public AudioSource Player;
+    public GameObject pauseMenu;
+    public GameObject settingsMenu;
+    public bool gamePaused = false;
+
+    private Image pauseButtonImage;
+
+    void Start()
+    {
+        pauseButtonImage = gameObject.GetComponent<Image>();
+        pauseMenu.SetActive(false);
+        settingsMenu.SetActive(false);
+        AlienBullets.mute = !AlienBullets.mute;
+        playerBullets.mute = !playerBullets.mute;
+        Earth.mute = !Earth.mute;
+        Player.mute = !Player.mute;
+    }
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ContinueButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void PauseGame()
+    {
+        if(pauseButtonImage == null)
+        {
+            Start();
+        }
+
+        if (!gamePaused)
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+            gamePaused = true;
+            pauseButtonImage.sprite = playSprite;
+        }
+        //else if (gamePaused)
+        //{
+        //    pauseMenu.SetActive(false);
+        //    Time.timeScale = 1;
+        //    gamePaused = false;
+        //    pauseButtonImage.sprite = pauseSprite;
+        //}
+    }
+    public void ResumeGame()
+    {
+        if (gamePaused)
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1;
+            gamePaused = false;
+            pauseButtonImage.sprite = pauseSprite;
+        }
+    }
+
+    public void OpenSettingsMenu()
+    {
+        settingsMenu.SetActive(true);
+        pauseMenu.SetActive(false);
+    }
+    public void CloseSettingsMenu()
+    {
+        settingsMenu.SetActive(false);
+        pauseMenu.SetActive(true);
+
+    }
+
+    public void MusicToggle()
+    {
+        Audio.mute = !Audio.mute;
+    }
+
+    public void AudioToggle()
+    {
+        AlienBullets.mute = !AlienBullets.mute;
+        playerBullets.mute = !playerBullets.mute;
+        Earth.mute = !Earth.mute;
+        Player.mute = !Player.mute;
+    }
+
+    public void AccelerometerToggle()
+    {
+        AudioListener.pause = !AudioListener.pause;
+    }
+}
