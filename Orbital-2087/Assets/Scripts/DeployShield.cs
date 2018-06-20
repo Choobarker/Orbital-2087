c1f29bf875c432e94ebaca360d8ced05ed9daa8f
Aidan Milne-Reeves <svx5484@autuni.ac.nz>
2018-06-21 10:04:54 +1200

    Earth Shield Button
    Added a temporary Earth shield button
    Just testing things atm.

---
+++ Orbital-2087/Assets/Scripts/DeployShield.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeployShield : MonoBehaviour {
+
+	public GameObject earthShield;
+	public Transform player;
+    Vector3 shieldPos;
+
+    void Start()
+    {
+        shieldPos = new Vector3(player.position.x, player.position.y - 10, 0);
+    }
+
+    public void Deploy()
+    {
+        if (shieldPos == null)
+        {
+            Start();
+        }
+        Instantiate(earthShield, player);
+    }
+}
